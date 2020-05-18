5744a9568789bdd6e0d316673db062a9b3b440c8
Aron Lebani <aron.lebani@gmail.com>
2020-05-19 00:14:54 +1000

    Template and pdf render

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+const generatePdf = async () => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/export/html', { waitUntil: 'networkidle0' });
+
+  const buffer = await page.pdf({ format: 'A4' });
+
+  await browser.close();
+  
+  return buffer;
+}
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+
+app.get('/export/html', (req, res) => {
+    const data = { };
+    res.render('template.html', data);
+});
+
+app.get('/export/pdf', (req, res) => {
+  generatePdf().then(buffer => {
+    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': buffer.length });
+    res.send(buffer);
+  });
+});
+
+app.listen(3000);
