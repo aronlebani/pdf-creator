9edf2792bac7d62cda3d190e2a45d01a9374775c
Aron Lebani <aron.lebani@gmail.com>
2020-05-19 19:49:44 +1000

    Refactor

---
+++ invoice.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const formatCurrency = (amount) => {
+  return `$${(Math.round(amount * 100) / 100).toFixed(2)}`;
+}
+
+const generatePdf = async () => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/export/html', { waitUntil: 'networkidle0' });
+
+  const buffer = await page.pdf({ format: 'A4' });
+
+  await browser.close();
+  
+  return buffer;
+}
+
+const getData = (configPath) => {
+  const data = JSON.parse(fs.readFileSync('./config.json'));
+
+  const totals = data.items.map(item => item.hours * item.rate);
+  const subTotal = totals.reduce((a, b) => a + b);
+
+  data.subTotal = formatCurrency(subTotal);
+  data.gst = formatCurrency(subTotal * 0.1);
+  data.total = formatCurrency(subTotal * 1.1);
+  data.items.forEach((item, index) => {
+    item.total = formatCurrency(totals[index]);
+  });
+
+  return data;
+}
+
+module.exports = { generatePdf, getData };
--- server.js
+++ server.js
@@ -1,41 +1,10 @@
 const path = require('path');
-const fs = require('fs');
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const puppeteer = require('puppeteer');
+
+const { generatePdf, getData } = require('./invoice');
 
 const app = express();
-
-const generatePdf = async () => {
-  const browser = await puppeteer.launch();
-
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000/export/html', { waitUntil: 'networkidle0' });
-
-  const buffer = await page.pdf({ format: 'A4' });
-
-  await browser.close();
-  
-  return buffer;
-}
-
-const formatCurrency = (amount) => {
-  return `$${(Math.round(amount * 100) / 100).toFixed(2)}`;
-}
-
-const deriveData = (data) => {
-  const totals = data.items.map(item => item.hours * item.rate);
-  const subTotal = totals.reduce((a, b) => a + b);
-
-  data.subTotal = formatCurrency(subTotal);
-  data.gst = formatCurrency(subTotal * 0.1);
-  data.total = formatCurrency(subTotal * 1.1);
-  data.items.forEach((item, index) => {
-    item.total = formatCurrency(totals[index]);
-  });
-
-  return data;
-}
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
@@ -43,7 +12,7 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/export/html', (req, res) => {
-  const data = deriveData(JSON.parse(fs.readFileSync('./config.json')));
+  const data = getData('./config.json');
   res.render('template.html', data);
 });
 
