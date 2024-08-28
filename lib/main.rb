3b79e05bc3759d200d75d77129b8d8f3893d301e
Aron Lebani <aron@lebani.dev>
2024-08-29 09:43:20 +1000

    Formatting

--- lib/main.rb
+++ lib/main.rb
@@ -10,7 +10,7 @@ CURRENCY = 'AUD'
 TAX_RATE = 0.1
 
 def format_currency(amount)
-  sprintf('%.2f', amount)
+  sprintf '%.2f', amount
 end
 
 def make_invoice_model(data)
@@ -24,68 +24,74 @@ def make_invoice_model(data)
     items: items.map do |i|
       {
         **i,
-        rate: format_currency(i[:rate]),
-        total: format_currency(i[:total]),
+        rate: format_currency i[:rate],
+        total: format_currency i[:total],
       }
     end,
-    subtotal: format_currency(sub_total),
-    gst: data[:gstinclusive] == true ? 'Included' : format_currency(gst),
-    total: format_currency(total),
+    subtotal: format_currency sub_total,
+    gst: data[:gstinclusive] == true ? 'Included' : format_currency gst,
+    total: format_currency total,
     currency: CURRENCY,
   }
 end
 
 def invoice_html(config_path)
-  raw = Tomlrb.parse(File.read(config_path), symbolize_keys: true)
-  model = make_invoice_model(raw)
+  raw = File.read config_path
+  toml = Tomlrb.parse raw, symbolize_keys: true
+  model = make_invoice_model toml
 
-  ERB.new(File.read('lib/invoice.erb')).result_with_hash(model)
+  erb = ERB.new File.read 'lib/invoice.erb'
+  erb.result_with_hash model
 end
 
 def md_html(file_path)
-  raw = File.read(file_path)
+  raw = File.read file_path
 
   # UNSAFE allows raw HTML which allows styling by embedding <style> tags in
   # the markdown
-  CommonMarker.render_html(raw, %i[HARDBREAKS UNSAFE])
+  CommonMarker.render_html raw, %i[HARDBREAKS UNSAFE]
 end
 
 def to_pdf(html, out)
   buffer = Puppeteer.launch do |browser|
     page = browser.new_page
-    page.set_content(html)
-    page.pdf(transferMode: 'ReturnAsStream')
+    page.set_content html
+    page.pdf transferMode: 'ReturnAsStream'
   end
 
-  File.binwrite(out, buffer)
+  File.binwrite out, buffer
+end
+
+def main(args)
+  html = if args[:config]
+    invoice_html args[:config]
+  elsif args[:md]
+    md_html args[:md]
+  end
+
+  to_pdf html, args[:out] || 'out.pdf'
 end
 
 if __FILE__ == $0
-  parser = OptionParser.new do |opts|
-    opts.banner = 'Usage: mkinvoice --config | --md [OPTIONS]'
-    opts.on('-c', '--config=CONFIG', 'configuration file')
-    opts.on('-m', '--md=MD', 'markdown file')
-    opts.on('-o', '--out=OUT', 'output file')
-  end
+  args = {}
+  parser = OptionParser.new
 
-  options = {}
-  parser.parse!(into: options)
+  parser.banner = 'Usage: mkinvoice --config | --md [OPTIONS]'
+  parser.on '-c', '--config=CONFIG', 'configuration file'
+  parser.on '-m', '--md=MD', 'markdown file'
+  parser.on '-o', '--out=OUT', 'output file'
 
-  unless options[:config] || options[:md]
-    puts('Missing required option --config or --md')
+  parser.parse! into: args
+
+  unless args[:config] || args[:md]
+    puts 'Missing required option --config or --md'
     exit 1
   end
 
-  unless File.exist?(options[:config])
-    puts("No such file: #{options[:config]}")
+  unless File.exist? args[:config]
+    puts "No such file: #{args[:config]}"
     exit 1
   end
 
-  html = if options[:config]
-    invoice_html(options[:config])
-  elsif options[:md]
-    md_html(options[:md])
-  end
-
-  to_pdf(html, options[:out] || 'out.pdf')
+  main args
 end
